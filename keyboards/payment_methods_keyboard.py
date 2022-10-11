24950fe0c223dd0ef52bf347b5ee892dfeadbd2f
abdoollokh <64684223+abdoollokh@users.noreply.github.com>
2022-10-11 18:57:21 +0500

    Update payment_methods_keyboard.py

--- keyboards/payment_methods_keyboard.py
+++ keyboards/payment_methods_keyboard.py
@@ -3,5 +3,6 @@ from aiogram import types
 keyboard = types.InlineKeyboardMarkup(1)
 qiwi = types.InlineKeyboardButton('🥝 QIWI | 💳 karta', callback_data='method_qiwi')
 yoo = types.InlineKeyboardButton('YOOMONEY | 💳 karta', callback_data='method_yoo')
+uzp = types.InlineKeyboardButtontypes.InlineKeyboardButton(text='UzCard/Humo | 💳 karta', url=f't.me/{username}', callback_data='help')
 back = types.InlineKeyboardButton('Orqaga', callback_data='cancel_payment')
-keyboard.add(qiwi, yoo, back)
+keyboard.add(qiwi, yoo, uzp, back)

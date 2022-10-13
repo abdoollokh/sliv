b0edc0adc38dc466b823bdd1ba166bacfb68558a
abdoollokh <64684223+abdoollokh@users.noreply.github.com>
2022-10-13 16:04:51 +0500

    Update search_result_keyboard.py

--- keyboards/search_result_keyboard.py
+++ keyboards/search_result_keyboard.py
@@ -1,7 +1,7 @@
 from aiogram import types
 
 keyboard = types.InlineKeyboardMarkup(1)
-archive = types.InlineKeyboardButton('💳 Arxiv sotib olish', callback_data='buy_archive')
+archive = types.InlineKeyboardButton('💳 Arxiv (barcha rasm va videolar) sotib olish', callback_data='buy_archive')
 photo = types.InlineKeyboardButton('🍑 Bitta rasm sotib olish', callback_data='buy_photo')
 phone = types.InlineKeyboardButton('📱 Telefon raqamini topish', callback_data='buy_phone')
 messages = types.InlineKeyboardButton('🔞 Yozishmalarni sotib olish', callback_data='buy_messages')
